df084d6232790734fc4c98e65b5db85c99893ac5
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2024-05-08 00:58:11 +0800

    fix: welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,8 @@
     <title>Backend Portal - PalmCo.de</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    @php $rand = Str::random(9); @endphp
+    <link rel="stylesheet" href="style.css?v={{ $rand }}">
 </head>
 <body>
     <div class="container">
